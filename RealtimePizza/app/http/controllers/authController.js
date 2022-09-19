647a9ede571bc353bd43a0599b79b22d0394d784
Kumar Saurav <kumarsauravks@gmail.com>
2022-09-19 14:58:52 +0530

    auth system

--- RealtimePizza/app/http/controllers/authController.js
+++ RealtimePizza/app/http/controllers/authController.js
@@ -1,10 +1,77 @@
+const User=require('../../models/user');
+const bcrypt=require('bcrypt');
+const passport=require('passport')
 function authController(){
     return {
         login(req,res){
             res.render('auth/login')
         },
+        postLogin(req,res,next){
+            const {email,password}=req.body;
+            //validate
+            if(!email || !password){
+                req.flash('error','All fields are required.')
+                return res.redirect('/login');
+            }
+            passport.authenticate('local',(err,user,info)=>{
+                if(err){
+                    req.flash('error',info.message)
+                    return next(err)
+                }
+                if(!user){
+                    req.flash('error',info.message)
+                    return res.redirect('/login')
+                }
+                req.login(user,(err)=>{
+                    if(err){
+                        req.flash('error',info.message)
+                        return next(err)
+                    }
+                    return res.redirect('/')
+                })
+            })(req,res,next)
+        },
         register:(req,res)=>{
             res.render('auth/register')
+        },
+        async postRegister(req,res){
+            //console.log(req.body)
+            const {name,email,password}=req.body;
+            //validate
+            if(!name || !email || !password){
+                req.flash('error','All fields are required.')
+                req.flash('name',name)
+                req.flash('email',email)
+                return res.redirect('/register');
+            }
+            //check if email already exist
+            User.exists({email:email},(err,result)=>{
+                if(result){
+                    req.flash('error','Email Already taken.')
+                    req.flash('name',name)
+                    req.flash('email',email)
+                    return res.redirect('/register');
+                }
+            })
+            //hash password
+            const hashPassword=await bcrypt.hash(password,10)
+            //create a user
+            const user=new User({
+                name,email,
+                password:hashPassword
+            })
+            user.save().then((user)=>{
+                //login
+                return res.redirect('/')
+            }).catch(err=>{
+                req.flash('error','Something went wrong')
+                return res.redirect('/register')
+            })
+        },
+        logout(req,res){
+            req.logout(()=>{
+                return res.redirect('/login');
+            })            
         }
     }
 }
--- RealtimePizza/server.js
+++ RealtimePizza/server.js
@@ -9,6 +9,7 @@ const path=require('path');
 const session=require('express-session');
 const flash=require('express-flash');
 const MongoDbStore=require('connect-mongo')
+const passport=require('passport')
 
 const mongoose=require('mongoose');
 //Db Connection
@@ -19,6 +20,7 @@ connection.on('error',console.error.bind(console,'connection error'));
 connection.once('open',()=>{
     console.log('DB Connected...!');
 });
+
 //session Store
 let mongoStore=new MongoDbStore({
     mongoUrl:url,
@@ -36,10 +38,17 @@ app.use(session({
 
 app.use(flash());
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+//Passport config
+const passportInit=require('./app/config/passport')
+passportInit(passport);
+app.use(passport.initialize())
+app.use(passport.session())
 
 //Global middleware
 app.use((req,res,next)=>{
     res.locals.session=req.session
+    res.locals.user=req.user
     next();
 })
 //set template engine
